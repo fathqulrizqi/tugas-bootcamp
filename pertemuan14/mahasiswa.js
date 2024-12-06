e693bba2700171ec653135fc5face94021bb65bd
hiphipdora <faaay.adistiii@gmail.com>
2024-12-06 15:23:02 +0700

    menambahkan tugas pertemuan 14, membuat file index dan mahasiswa js

---
+++ pertemuan14/mahasiswa.js
@@ -0,0 +1,29 @@
+class Mahasiswa {
+    constructor(nim, nama, jenis_kelamin, ipk){
+        this.nim = nim;
+        this.nama = nama;
+        this.jenis_kelamin = jenis_kelamin;
+        this.ipk = ipk;
+    }
+
+    
+    predikat(){
+        const ipeka = this.ipk
+
+        if (ipeka > 0 && ipeka < 2.00){
+            return 'Cukup';
+        }
+        else if (ipeka >= 2.00 && ipeka < 3.00) {
+            return 'Baik';
+        }
+        else if (ipeka >= 3.00 && ipeka < 3.75) {
+            return 'Memuaskan';
+        }
+        else if (ipeka >= 3.75 && ipeka <=4) {
+            return 'CumLaude';
+        }
+        else {
+            return 'Tidak Valid!';
+        }
+    }
+}
